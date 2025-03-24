fbc559770f5d1e7112e4f2f95c50064bb788e059
yash0632 <yashsharma0632@gmail.com>
2025-03-25 02:18:00 +0530

    updated_zap_restapi

--- primary-backend/src/routes/zaprouter.ts
+++ primary-backend/src/routes/zaprouter.ts
@@ -7,6 +7,27 @@ const prisma = new PrismaClient();
 
 
 router.post("/",authMiddleware,async(req,res)=>{
+    const userEmail = req.user?.email;
+    if(!userEmail){
+        res.status(401).json({
+            message:"Unauthorized"
+        })
+        return;
+    }
+    const userId = await prisma.user.findUnique({
+        where:{
+            email:userEmail
+        },
+        select:{
+            id:true
+        }
+    })
+    if(!userId){
+        res.status(401).json({
+            message:"Unauthorized"
+        })
+        return;
+    }
     const response = ZapCreateSchema.safeParse(req.body);
     if(!response.success){
         res.status(400).json({
@@ -22,7 +43,9 @@ router.post("/",authMiddleware,async(req,res)=>{
 
     const zap = await prisma.$transaction(async(tx)=>{
         const zap = await tx.zap.create({
-            data:{}
+            data:{
+                userId:userId.id
+            }
         });
 
         const triggerTable = await tx.trigger.create({
@@ -50,18 +73,89 @@ router.post("/",authMiddleware,async(req,res)=>{
 
     res.json({
         message:"Zap Created Successfully",
-        zap
+        zap:zap
     })
     return;
     
 })
 
-router.get("/",authMiddleware,(req,res)=>{
-    console.log("zap handler");
+router.get("/",authMiddleware,async(req,res)=>{
+    //Get all the wraps for the user
+    try{
+        const userEmail = req.user?.email;
+        if(!userEmail){
+            res.status(401).json({
+                message:"Unauthorized"
+            })
+            return;
+        }
+        const user =await prisma.user.findUnique({
+            where:{
+                email:userEmail
+            },
+            include:{
+                zaps:true
+            }
+        })
+        if(!user){
+            res.status(401).json({
+                message:"Unauthorized"
+            })
+            return;
+        }
+        const zaps = user.zaps;
+        
+        res.status(200).json({
+            message:"Zaps fetched successfully",
+            zaps:zaps
+        })
+        return;
+    }
+    catch(error){
+        res.status(500).json({
+            message:"Internal Server Error"
+        })
+        return;
+    }
 })
 
-router.get("/:zapId",authMiddleware,(req,res)=>{
-    console.log("get a zap");
+router.get("/:zapId",authMiddleware,async(req,res)=>{
+    //Get a zap by id
+    try{
+        const zapParamsId = req.params.zapId;
+        const user = await prisma.user.findUnique({
+            where:{
+                email:req.user?.email
+            },
+            include:{
+                zaps:true
+            }
+        })
+        if(!user){
+            res.status(401).json({
+                message:"Unauthorized"
+            })
+            return;
+        }
+        const zap = user.zaps.find(zap=>zap.id===zapParamsId);
+        if(!zap){
+            res.status(404).json({
+                message:"Zap not found"
+            })
+            return;
+        }
+        res.status(200).json({
+            message:"Zap fetched successfully",
+            zap:zap
+        })
+        return;
+    }
+    catch(error){
+        res.status(500).json({
+            message:"Internal Server Error"
+        })
+        return;
+    }
 })
 
 
